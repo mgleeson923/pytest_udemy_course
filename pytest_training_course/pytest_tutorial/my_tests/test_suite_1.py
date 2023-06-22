8c08b7c5d7628e16ee40f971b6eb4aad86081d7f
mgleeson923 <mgleeson923@gmail.com>
2023-06-22 15:22:40 -0400

    First Commit - Setup of Pytest, Python 3, virtualenvs, etc.

---
+++ pytest_training_course/pytest_tutorial/my_tests/test_suite_1.py
@@ -0,0 +1,13 @@
+def test_login_page_valid_user():
+    print("Logging in with valid user")
+    print("function: aaaaa")
+
+
+def test_login_page_wrong_password():
+    print("Login with wrong password")
+    print("Function: bbbbbbbbb")
+
+
+
+
+
---
+++ pytest_training_course/pytest_tutorial/my_tests/test_suite_2.py
@@ -0,0 +1,9 @@
+class TestCheckout(object):
+
+    def test_checkout_as_guest(self):
+        print("Checkout as guest")
+        print("Class: 11111")
+
+    def test_checkout_with_existing_user(self):
+        print("checkout with existing user")
+        print("Class: 222222")
