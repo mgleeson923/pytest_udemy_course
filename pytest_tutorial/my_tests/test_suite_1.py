aa6799a7789b57a9a69684ec912302534dd4fe8c
mgleeson923 <mgleeson923@gmail.com>
2023-06-22 21:31:12 -0400

    Created pytest config file.

--- pytest_tutorial/my_tests/test_suite_1.py
+++ pytest_tutorial/my_tests/test_suite_1.py
@@ -6,6 +6,7 @@ def test_login_page_valid_user():
     print("Logging in with valid user")
     print("function: aaaaa")
 
+
 @pytest.mark.regression
 def test_login_page_wrong_password():
     print("Login with wrong password")
