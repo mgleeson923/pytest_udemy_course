6c5f1d07cbbc279b9bec1493397770f895b5b99b
mgleeson923 <mgleeson923@gmail.com>
2023-06-28 14:48:29 -0400

    Created Setup file and src folders.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='ssqaapitest',
+      version='1.0',
+      description="Practice API Testing",
+      author='Michael Gleeson',
+      author_email='spartanzealot9@gmail.com',
+      url='https://dummyurl.com',
+      packages=find_packages()
+      )
