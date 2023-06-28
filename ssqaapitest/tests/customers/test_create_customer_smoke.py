4c84ac5c815166a9621364739f2cedf3b1b87f92
mgleeson923 <mgleeson923@gmail.com>
2023-06-28 16:26:23 -0400

    Created config files and test template.

---
+++ ssqaapitest/tests/customers/test_create_customer_smoke.py
@@ -0,0 +1,23 @@
+
+import pytest
+import logging as logger
+
+
+
+@pytest.mark.tcid29
+def test_create_customer_only_email_password():
+
+    logger.info("Test: Create New Customer with Email and Password Only")
+
+    email = ''
+    password = ''
+
+    # create payload
+
+    # make call
+
+    # verify status code
+
+    # verify email in response
+
+    # verify customer is created in database
