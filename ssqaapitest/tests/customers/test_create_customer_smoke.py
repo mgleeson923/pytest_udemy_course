3931a4188e4538e9d8882a5fb8f747b4d9a8151f
mgleeson923 <mgleeson923@gmail.com>
2023-06-28 16:44:03 -0400

    Created Helper Utlities dictionary, along with email + password generation file.

--- ssqaapitest/tests/customers/test_create_customer_smoke.py
+++ ssqaapitest/tests/customers/test_create_customer_smoke.py
@@ -1,6 +1,7 @@
 
 import pytest
 import logging as logger
+from ssqaapitest.src.utlities.genericUtilities import generate_random_email_and_password
 
 
 
@@ -9,10 +10,13 @@ def test_create_customer_only_email_password():
 
     logger.info("Test: Create New Customer with Email and Password Only")
 
-    email = ''
-    password = ''
+    rand_info = generate_random_email_and_password()
+
+    email = rand_info['email']
+    password = rand_info['password']
 
     # create payload
+    payload = {'email': email, 'password': password}
 
     # make call
 
