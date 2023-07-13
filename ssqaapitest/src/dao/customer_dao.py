7e048e1b05f2cf69863b584a54d64ab6106eab35
mgleeson923 <mgleeson923@gmail.com>
2023-07-13 15:02:07 -0400

    Created Database Helper Files and verified creation of User in DB

---
+++ ssqaapitest/src/dao/customer_dao.py
@@ -0,0 +1,18 @@
+from ssqaapitest.src.utlities.dbUtilities import DBUtility
+
+
+class Customer_DAO(object):
+
+    def __init__(self):
+        self.db_helper = DBUtility()
+
+    def get_customer_by_email(self, email):
+
+        sql = f"SELECT * FROM local.wp_users WHERE user_email = '{email}';"
+        rs_sql = self.db_helper.execute_select(sql)
+
+        import pdb; pdb.set_trace()
+
+
+
+
--- ssqaapitest/tests/customers/test_create_customer_smoke.py
+++ ssqaapitest/tests/customers/test_create_customer_smoke.py
@@ -2,6 +2,7 @@ import pytest
 import logging as logger
 from ssqaapitest.src.utlities.genericUtilities import generate_random_email_and_password
 from ssqaapitest.src.helpers.customer_helper import CustomerHelper
+from ssqaapitest.src.dao.customer_dao import Customer_DAO
 
 
 @pytest.mark.tcid29
@@ -24,7 +25,10 @@ def test_create_customer_only_email_password():
     assert cust_api_info['email'] == email, f"Create Customer API Return Wrong Email. Email:{email}"
     assert cust_api_info['first_name'] == '', f"Create Customer API returned value for firstname, but should be empty."
 
+    # verify customer is created in database
+
+    cust_dao = Customer_DAO()
+    cust_info = cust_dao.get_customer_by_email(email)
+
     import pdb;
     pdb.set_trace()
-
-    # verify customer is created in database
